2b098fd159cebcfa4537a1eeb0227a2706a6e0b7
Victor Mudaki Mwachi <20131567+VictorMwachi@users.noreply.github.com>
2023-06-26 15:06:01 +0300

    1-block-scoped.js

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,8 +3,8 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
-    const task = true;
-    const task2 = false;
+    const task = true; //no-unused-vars
+    const task2 = false; //no-unused-vars
   }
 
   return [task, task2];
