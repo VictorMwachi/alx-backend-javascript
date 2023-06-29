21f71fd6c1303a78fd1e8e6578fa7f98b364d8dd
Victor Mudaki Mwachi <20131567+VictorMwachi@users.noreply.github.com>
2023-06-29 11:14:18 +0300

    Update 0-classroom.js

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,4 +1,11 @@
+/**
+* class Classroom
+*/
 export default class ClassRoom {
+  /**
+  * @param {Number} maxStudentsSize - max student class
+  * can accommodate
+  */
   constructor(maxStudentsSize) {
     this._maxStudentsSize = maxStudentsSize;
   }
