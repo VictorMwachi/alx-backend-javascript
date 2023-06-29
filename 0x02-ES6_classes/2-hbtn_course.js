f442f60fe0002e3d5c8415e5f71290f1eed0f3f0
Victor Mwachi <mudakizevictor@gmail.com>
2023-06-29 13:39:05 +0000

    blessings of eslinter

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -3,63 +3,62 @@
 */
 export default class HolbertonCourse {
   /**
-  * 
-  */
+   *
+   */
   constructor(name, length, students) {
-  this._name = name;
-  this._length = length;
-  this._students = students;
+    this._name = name;
+    this._length = length;
+    this._students = students;
   }
-  
+
   /**
-  * gets name
-  */
+   * gets name
+   */
   get name() {
     return this._name;
   }
-  
+
   /**
-  * sets the name of course
-  */
-  
+   * sets the name of course
+   */
+
   set name(val) {
     if (typeof val !== 'string') {
       throw new TypeError('Name must be a string');
     }
     this._name = val;
   }
-  
+
   /**
-  * gets length
-  */
-  
+   * gets length
+   */
+
   get length() {
     return this._length;
   }
-  
+
   /**
-  * sets the length of course
-  */
-  
+   * sets the length of course
+   */
+
   set length(val) {
     if (typeof val !== 'number') {
       throw new TypeError('Length must be a number');
     }
     this._length = val;
   }
-  
+
   /**
-  * gets an array students names
-  * in this course
-  */
-  
+   * gets an array students names
+   * in this course
+   */
+
   get students() {
     return this._students;
   }
-  
   /**
-  * sets students names in the course
-  */
+   * sets students names in the course
+   */
 
   set students(list) {
     if (!(list instanceof Array)) {
@@ -72,4 +71,4 @@ export default class HolbertonCourse {
     });
     this._students = list;
   }
-}   
+}
