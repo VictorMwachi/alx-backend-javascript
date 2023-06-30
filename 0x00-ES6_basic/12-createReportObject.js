70030b608b2508f65d4106be6637f2b2d9408e15
Victor Mwachi <mudakizevictor@gmail.com>
2023-06-30 11:31:15 +0000

    12-createReportObject.js

---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,10 @@
+export default function createReportObject(employeesList) {
+	const report = {
+		const allEmployees = {
+			[employeesList];
+		},
+		getNumberOfDepartments(object){
+			return [object].length;
+		};
+	};
+}
