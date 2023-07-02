43a93542dc7a5df0522ad5326e50961bd37db62d
Victor Mwachi <mudakizevictor@gmail.com>
2023-07-02 14:27:50 +0000

    eslint

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,40 +1,44 @@
-import Currency from './3-currency.js';
+import Currency from './3-currency';
 
-export default Pricing {
-	constructor(amount,currency) {
-		this._amount = amount;
-		this._currency = currency;
-	}
-	get amount(){
-		return this._amount;
-	}
-	set amount(value){
-		if(typeof value !== 'number'){
-			throw new TypeError('Amount must be a number');
-		};
-		this._amount = value;
-	}
-	get currency(){
-		return this._currency;
-	}
-	set currency(value){
-		if(!(value instanceof Currency)){
-			throw new TypeError('value must be of type currency');
-		}
-		this._currency = value;
-	}
-	displayFullPrice(){
-		return `${_amount} ${this._currency.name} (${this._currency.code})`;
-	}
-	static convertPrice(amount,conversionRate){
-		if(typeof amount !=='number'){
-			throw new TypeError('Amount must be a number');
-		}
-		if(typeof conversionRate !== 'number'){
-			throw new TypeError('Conversion Rate Must be anumber');
-		}
-		return amount * conversionRate;
-	}
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('Amount must be a number');
+    }
+    this._amount = value;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(value) {
+    if (!(value instanceof Currency)) {
+      throw new TypeError('value must be of type currency');
+    }
+    this._currency = value;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    if (typeof amount !== 'number') {
+      throw new TypeError('Amount must be a number');
+    }
+    if (typeof conversionRate !== 'number') {
+      throw new TypeError('Conversion Rate Must be anumber');
+    }
+    return amount * conversionRate;
+  }
 }
-		
-
--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,18 +1,21 @@
 export default class Building {
-	constructor(sqft){
-		this._sqft = sqft;
-		if(this.constructor !== 'Building'){
-			if(typeof this.evacuationWarningMessage !== 'function'){
-				throw new TypeError('Class extending Building must override evacuationWarningMessage');
-			}
-		}
-	}
-	get sqft(){
-		return this._sqft;
-	}
-	set sqft(vaule){
-		if(typeof value !== 'number'){
-			throw new TypeError('Sqft must be a number!');
-		}
-	}
+  constructor(sqft) {
+    this._sqft = sqft;
+    if (this.constructor !== 'Building') {
+      if (typeof this.evacuationWarningMessage !== 'function') {
+        throw new TypeError('Class extending Building must override evacuationWarningMessage');
+      }
+    }
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('Sqft must be a number!');
+    }
+    this._sqft = value;
+  }
 }
