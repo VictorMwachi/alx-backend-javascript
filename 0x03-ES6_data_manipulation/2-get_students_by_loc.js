340b54ece1b3a29156ccdb0d836cc0923ac11658
Victor Mwachi <mudakizevictor@gmail.com>
2023-07-03 20:41:21 +0000

    getstudent by location

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,7 +1,3 @@
 export default function getStudentsByLocation(students, city) {
-  if (students instanceof Array) {
-    students.filter((student) => student.location === city);
-  } else {
-    return [];
-  }
+  return students.filter((student) => student.location === city);
 }
