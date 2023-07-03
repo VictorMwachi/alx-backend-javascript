26640105d7f49374de7c76f36b5c57e2135066b6
victormwachi <mudakizevictor@gmail.com>
2023-07-03 22:38:57 +0300

    9-groceries_list.js

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,9 @@
+export default function groceriesList() {
+	const map = new Map();
+	map.set('Apples', 10);
+	map.set('Tomatoes', 10);
+	map.set('Pasta', 1);
+	map.set('ice', 1);
+	map.set('Banana', 5);
+	return map;
+}
