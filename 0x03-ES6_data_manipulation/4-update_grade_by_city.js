4ab4c324b872161d4fe1b6245f3f76f6377a09ad
Victor Mwachi <mudakizevictor@gmail.com>
2023-07-04 05:58:59 +0000

    5-typed_arrays.js

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,5 +1,4 @@
 function updateStudentGradeByCity(students, city,newGrades){
-	if (students instanceof Array && newGrades instanceof Array) {
 		students.filter((student) => student.location === city)
 		.map((student)=> {
 			const studentsGrades = newGrades.filter(studentId === student.id));}
