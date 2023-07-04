0b072502364464f9a39481fafcd17134897f56b7
Victor Mwachi <mudakizevictor@gmail.com>
2023-07-04 08:03:04 +0000

    student grades

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,6 +1,11 @@
-function updateStudentGradeByCity(students, city,newGrades){
-		students.filter((student) => student.location === city)
-		.map((student)=> {
-			const studentsGrades = newGrades.filter(studentId === student.id));}
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  students.filter((student) => student.location === city)
+    .map((student) => {
+      const studentsGrades = newGrades.filter((grade) => grade.studentId === student.id);
+      const grade = studentsGrades.length ? studentsGrades[0].grade : 'N/A';
+      return {
+        ...student,
+        grade,
+      };
+    });
 }
-
