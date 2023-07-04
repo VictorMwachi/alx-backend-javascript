ac9c376086013867240fb2a0ca647489195aa32d
Victor Mwachi <mudakizevictor@gmail.com>
2023-07-04 08:18:14 +0000

    8-clean_set.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,6 +2,7 @@ export default function cleanSet(set, startString) {
   if (startString === '') {
     return [].join('-');
   }
+
   const startStringArray = Array.from(set).filter((item) => item.match(startString));
   return startStringArray.map((item) => item.replace(startString, '')).join('-');
 }
