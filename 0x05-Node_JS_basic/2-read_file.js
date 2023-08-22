664058ba88133fc73683b36319aeeb4711f42947
Victor Mwachi <mudakizevictor@gmail.com>
2023-08-22 09:04:41 +0000

    syncyfile read

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+/**
+ *@param {String} path The path to the CSV data file.
+ */
+
+const countStudents = (path) => {
+  if (!fs.existsSync(path)) {
+    throw new Error('Cannot load the database');
+  }
+  if (!fs.statSync(path).isFile()) {
+    throw new Error('Cannot load the database');
+  }
+  const lines = fs
+    .readFileSync(path, 'utf-8')
+    .toString('utf-8')
+    .trim()
+    .split('\n');
