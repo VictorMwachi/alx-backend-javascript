f5732e8e586d9a8177835fb5cd205045a58a00f5
Victor Mwachi <mudakizevictor@gmail.com>
2023-08-22 07:14:10 +0000

    eslint

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -2,6 +2,6 @@
 * @param {String} message The message to display.
 */
 const displayMessage = (message) => {
-	console.log(message);
-}
+  console.log(message);
+};
 module.exports = displayMessage;
