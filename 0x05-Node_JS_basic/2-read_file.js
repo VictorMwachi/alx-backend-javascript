ce1b6648ffab527439d8b5319fdff57958bc65bc
Victor Mudaki Mwachi <20131567+VictorMwachi@users.noreply.github.com>
2023-08-25 13:00:30 +0300

    Update 2-read_file.js

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -33,7 +33,7 @@ const countStudents = (path) => {
   }
   console.log(`Number of students: ${data.length}`);
   Object.keys(studentList).forEach((key) => {
-    console.log(`Number of students in ${key}: ${studentGroups[key]}. List: ${studentList[key].join(' ')}`);
+    console.log(`Number of students in ${key}: ${studentGroups[key]}. List: ${studentList[key].join(', ')}`);
   });
 };
 module.exports = countStudents;
