fd3b6cdf344549a89d865472e6c384e1a8b47154
Victor Mwachi <mudakizevictor@gmail.com>
2023-08-25 09:52:19 +0000

    readfile syncronously

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -4,37 +4,36 @@ const fs = require('fs');
  */
 
 const countStudents = (path) => {
-	if (!fs.existsSync(path)) {
-		throw new Error('Cannot load the database');
-	}
-	if (!fs.statSync(path).isFile()) {
-		throw new Error('Cannot load the database');
-	}
-	const fileLines = fs
-		.readFileSync(path, 'utf-8')
-		.trim()
-		.split('\n');
-	const field = fileLines[0].split(",").indexOf("field")
-	const firstname = fileLines[0].split(",").indexOf("firstname")
-	const studentGroups = {};
-	const studentList = {};
-	const data = fileLines.slice(1);
-	for (idx in data){
-		const student = data[idx].split(",");
-		if(studentGroups[student[field]] === undefined){
-			studentGroups[student[field]] = 1;
-			if(studentList[student[field]] === undefined){
-				studentList[student[field]]=[student[firstname]]
-			}
-		}
-		else{
-			studentGroups[student[field]]++;
-			studentList[student[field]].push(student[firstname]);
-		}
-	}
-	console.log(`Number of students: ${data.length}`);
-	Object.keys(studentList).forEach((key) => {
-		console.log(`Number of students in ${key}: ${studentGroups[key]}. List: ${studentList[key]}`);
-	});
-	}
+  if (!fs.existsSync(path)) {
+    throw new Error('Cannot load the database');
+  }
+  if (!fs.statSync(path).isFile()) {
+    throw new Error('Cannot load the database');
+  }
+  const fileLines = fs
+    .readFileSync(path, 'utf-8')
+    .trim()
+    .split('\n');
+  const field = fileLines[0].split(',').indexOf('field');
+  const firstname = fileLines[0].split(',').indexOf('firstname');
+  const studentGroups = {};
+  const studentList = {};
+  const data = fileLines.slice(1);
+  for (let idx = 0; idx < data.length; idx += 1) {
+    const student = data[idx].split(',');
+    if (studentGroups[student[field]] === undefined) {
+      studentGroups[student[field]] = 1;
+      if (studentList[student[field]] === undefined) {
+        studentList[student[field]] = [student[firstname]];
+      }
+    } else {
+      studentGroups[student[field]] += 1;
+      studentList[student[field]].push(student[firstname]);
+    }
+  }
+  console.log(`Number of students: ${data.length}`);
+  Object.keys(studentList).forEach((key) => {
+    console.log(`Number of students in ${key}: ${studentGroups[key]}. List: ${studentList[key]}`);
+  });
+};
 module.exports = countStudents;
