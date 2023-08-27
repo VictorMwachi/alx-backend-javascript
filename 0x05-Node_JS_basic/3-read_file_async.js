52bea864420b9d0ae93c1fc84c549987ffd5e620
victormwachi <mudakizevictor@gmail.com>
2023-08-27 16:17:44 +0300

    asycnronus

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -3,37 +3,39 @@ const fs = require('fs');
  *@param {String} path The path to the CSV data file.
  */
 
-const countStudents = (path) => {
-  if (!fs.existsSync(path)) {
-    throw new Error('Cannot load the database');
-  }
-  if (!fs.statSync(path).isFile()) {
-    throw new Error('Cannot load the database');
-  }
-  const fileLines = fs
-    .readFileSync(path, 'utf-8')
-    .trim()
-    .split('\n');
-  const field = fileLines[0].split(',').indexOf('field');
-  const firstname = fileLines[0].split(',').indexOf('firstname');
-  const studentGroups = {};
-  const studentList = {};
-  const data = fileLines.slice(1);
-  for (let idx = 0; idx < data.length; idx += 1) {
-    const student = data[idx].split(',');
-    if (studentGroups[student[field]] === undefined) {
-      studentGroups[student[field]] = 1;
-      if (studentList[student[field]] === undefined) {
-        studentList[student[field]] = [student[firstname]];
-      }
-    } else {
-      studentGroups[student[field]] += 1;
-      studentList[student[field]].push(student[firstname]);
-    }
-  }
-  console.log(`Number of students: ${data.length}`);
-  Object.keys(studentList).forEach((key) => {
-    console.log(`Number of students in ${key}: ${studentGroups[key]}. List: ${studentList[key].join(', ')}`);
-  });
-};
+const countStudents = (path) => new Promise((resolve, reject) => {
+    fs.read(path, 'utf-8', (err, data) => {
+	    if (err) {
+		    reject(throw new Error('Cannot load the database'));
+	    }
+	    if (data) {
+		    const fileLines = fs
+			    .readFileSync(path, 'utf-8')
+			    .trim()
+			    .split('\n');
+		    const field = fileLines[0].split(',').indexOf('field');
+		    const firstname = fileLines[0].split(',').indexOf('firstname');
+		    const studentGroups = {};
+		    const studentList = {};
+		    const data = fileLines.slice(1);
+		    for (let idx = 0; idx < data.length; idx += 1) {
+			    const student = data[idx].split(',');
+			    if (studentGroups[student[field]] === undefined) {
+				    studentGroups[student[field]] = 1;
+				    if (studentList[student[field]] === undefined) {
+					    studentList[student[field]] = [student[firstname]];
+				    }
+			    }
+			    else {
+				    studentGroups[student[field]] += 1;
+				    studentList[student[field]].push(student[firstname]);
+			    }
+		    }
+		    console.log(`Number of students: ${data.length}`);
+		    Object.keys(studentList).forEach((key) => {
+			    console.log(`Number of students in ${key}: ${studentGroups[key]}. List: ${studentList[key].join(', ')}`);
+		    });
+	    };
+    });
+}
 module.exports = countStudents;
