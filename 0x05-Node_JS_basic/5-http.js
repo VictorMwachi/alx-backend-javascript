15fac0f9e509f8ae28262438cb91a8843d69b298
victormwachi <mudakizevictor@gmail.com>
2023-08-27 19:36:37 +0300

    5-http.js

---
+++ 0x05-Node_JS_basic/5-http.js
@@ -0,0 +1,51 @@
+const http = require('http');
+const fs = require('fs');
+
+
+const countStudents = (path) => new Promise((resolve, reject) => {
+  fs.readFile(path, 'utf-8', (err, fdata) => {
+    if (err) {
+      reject(new Error('Cannot load the database'));
+    }
+    if (fdata) {
+      const fileLines = fdata.toString()
+        .trim()
+        .split('\n');
+      const field = fileLines[0].split(',').indexOf('field');
+      const firstname = fileLines[0].split(',').indexOf('firstname');
+      const studentGroups = {};
+      const studentList = {};
+      const data = fileLines.slice(1);
+      for (let idx = 0; idx < data.length; idx += 1) {
+        const student = data[idx].split(',');
+        if (studentGroups[student[field]] === undefined) {
+          studentGroups[student[field]] = 1;
+          if (studentList[student[field]] === undefined) {
+            studentList[student[field]] = [student[firstname]];
+          }
+        } else {
+          studentGroups[student[field]] += 1;
+          studentList[student[field]].push(student[firstname]);
+        }
+      }
+      console.log(`Number of students: ${data.length}`);
+      Object.keys(studentList).forEach((key) => {
+        console.log(`Number of students in ${key}: ${studentGroups[key]}. List: ${studentList[key].join(', ')}`);
+      });
+    }
+    resolve();
+  });
+});
+const app = http.createServer((req, res) => {
+	const url = req.url;
+	res.statusCode = 200;
+	res.setHeader('Content-Type', 'text/plain');
+	if(url ==='/'){
+		res.end('Hello Holberton School!');}
+	if(url ==='/students') {
+		console.log(process.argv.length);
+		res.end(process.argv[2])
+	}
+});
+app.listen(1245, '127.0.0.1');
+module.exports = app;
