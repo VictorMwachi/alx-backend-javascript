16f0d3ee33e169afb11a5967adbeb9e18963583f
Victor Mwachi <mudakizevictor@gmail.com>
2023-09-19 12:25:22 +0000

    3-all.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,7 +1,7 @@
 import { uploadPhoto, createUser } from "./utils.js";
 
 export default function handleProfileSignup() {
-  return Promise
+  return new Promise
     .all([uploadPhoto, createUser])
     .then((response) => {
       console.log(`${response[0].body} ${response[1].firstName} ${response[2].lastName}`);
