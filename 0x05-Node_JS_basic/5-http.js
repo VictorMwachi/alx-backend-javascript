9fe0b1041ad19381f00342401f3dcbc106ab2262
Victor Mudaki Mwachi <20131567+VictorMwachi@users.noreply.github.com>
2023-11-23 16:26:44 +0300

    Update 5-http.js

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,6 +1,39 @@
 const http = require('http');
 const fs = require('fs');
 const countStudents = (path) => new Promise((resolve, reject) => {
+  fs.readFile(path, 'utf-8', (err, fdata) => {
+    if (err) {
+      reject(new Error('Cannot load the database'));
+    }
+    if (fdata) {
+      const fileLines = fdata.toString()
+        .trim()
+        .split('\n');
+      const field = fileLines[0].split(',').indexOf('field');
+      const firstname = fileLines[0].split(',').indexOf('firstname');
+      const studentGroups = {};
+      const studentList = {};
+      const data = fileLines.slice(1);
+      for (let idx = 0; idx < data.length; idx += 1) {
+        const student = data[idx].split(',');
+        if (studentGroups[student[field]] === undefined) {
+          studentGroups[student[field]] = 1;
+          if (studentList[student[field]] === undefined) {
+            studentList[student[field]] = [student[firstname]];
+          }
+        } else {
+          studentGroups[student[field]] += 1;
+          studentList[student[field]].push(student[firstname]);
+        }
+      }
+      output += `Number of students: ${data.length}\n`;
+      Object.keys(studentList).forEach((key) => {
+        output += `Number of students in ${key}: ${studentGroups[key]}. List: ${studentList[key].join(', ')}\n`;
+      });
+      output += output.trim();
+      resolve(output);
+    }
+  });
 });
 
 const app = http.createServer((req, res) => {
