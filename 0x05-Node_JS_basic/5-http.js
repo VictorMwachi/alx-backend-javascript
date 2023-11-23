d96e7bc90367bd9eab3161a519b6c198e6c2d9a2
Victor Mudaki Mwachi <20131567+VictorMwachi@users.noreply.github.com>
2023-11-23 16:41:50 +0300

    Update 5-http.js

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,6 +1,5 @@
 const http = require('http');
 const fs = require('fs');
-let output = '';
 
 const countStudents = (path) => new Promise((resolve, reject) => {
   fs.readFile(path, 'utf-8', (err, fdata) => {
@@ -28,7 +27,7 @@ const countStudents = (path) => new Promise((resolve, reject) => {
           studentList[student[field]].push(student[firstname]);
         }
       }
-      output += `Number of students: ${data.length}\n`;
+      let output += `Number of students: ${data.length}\n`;
       Object.keys(studentList).forEach((key) => {
         output += `Number of students in ${key}: ${studentGroups[key]}. List: ${studentList[key].join(', ')}\n`;
       });
