2a813ac2ae92ec0c515e7b64d1652fd51b25418b
Victor Mwachi <mudakizevicor@gmsil.com>
2023-11-23 13:06:50 +0000

    eslint

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,20 +1,21 @@
 const http = require('http');
 const fs = require('fs');
-const countStudents = require('./3-read_file_async')
+const countStudents = require('./3-read_file_async');
+
 const app = http.createServer((req, res) => {
-	const url = req.url;
-	res.statusCode = 200;
-	res.setHeader('Content-Type', 'text/plain');
-	if(url ==='/'){
-		res.write('Hello Holberton School!');}
-	if(url ==='/students') {
-		countStudents(process.argv[2])
-		.then((output)=>{
-			res.write(output)
-			res.end();
-		})
-	}
-	
+  const { url } = req;
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  if (url === '/') {
+    res.write('Hello Holberton School!');
+  }
+  if (url === '/students') {
+    countStudents(process.argv[2])
+      .then((output) => {
+        res.write(output);
+        res.end();
+      });
+  }
 });
 app.listen(1245, '127.0.0.1');
 module.exports = app;
