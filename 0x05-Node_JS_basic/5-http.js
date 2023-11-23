62979916fbc5af6f39b3efba6e3fc131de28668c
Victor Mudaki Mwachi <20131567+VictorMwachi@users.noreply.github.com>
2023-11-23 14:46:49 +0300

    Update 5-http.js

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,43 +1,6 @@
 const http = require('http');
 const fs = require('fs');
-
-
-const countStudents = (path) => new Promise((resolve, reject) => {
-  fs.readFile(path, 'utf-8', (err, fdata) => {
-    if (err) {
-      reject(new Error('Cannot load the database'));
-    }
-    if (fdata) {
-      const fileLines = fdata.toString()
-        .trim()
-        .split('\n');
-      const field = fileLines[0].split(',').indexOf('field');
-      const firstname = fileLines[0].split(',').indexOf('firstname');
-      const studentGroups = {};
-      const studentList = {};
-      const data = fileLines.slice(1);
-      for (let idx = 0; idx < data.length; idx += 1) {
-        const student = data[idx].split(',');
-        if (studentGroups[student[field]] === undefined) {
-          studentGroups[student[field]] = 1;
-          if (studentList[student[field]] === undefined) {
-            studentList[student[field]] = [student[firstname]];
-          }
-        } else {
-          studentGroups[student[field]] += 1;
-          studentList[student[field]].push(student[firstname]);
-        }
-      }
-      process.stdout.write('This is the list of our students');
-      Object.keys(studentList).forEach((key) => {
-        process.stdout.write(`Number of students in ${key}: ${studentGroups[key]}. List: ${studentList[key].join(', ')}`);
-      });
-    }
-    resolve([process.stdout.write('This is the list of our students'),
-      Object.keys(studentList).forEach((key) => {
-        process.stdout.write(`Number of students in ${key}: ${studentGroups[key]}. List: ${studentList[key].join(', ')}`);
-      
-  })]);
+const countStudents = require('./4-http.js')
 const app = http.createServer((req, res) => {
 	const url = req.url;
 	res.statusCode = 200;
