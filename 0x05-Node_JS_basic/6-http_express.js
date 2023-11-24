b455a0a7c07870407bb3fe5a498077ac8f3e8d07
Victor Mudaki Mwachi <20131567+VictorMwachi@users.noreply.github.com>
2023-11-24 15:10:23 +0300

    Update 6-http_express.js

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -6,7 +6,7 @@ const express = require('express');
 const app = express();
 const port = 1245;
 app.get('/', (req, res) => {
-  res.send('Hello Holberton School!');
+  res.status(200).send('Hello Holberton School!');
 });
 app.listen(port);
 module.exports = app
